8f7f8cafb945c03aeee6ba4ec2c7f25654c3a884
vale690 <valeryscausilberrio@gmail.com>
2025-11-09 18:49:06 -0500

    script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Por ahora no es necesario código JS adicional.
+// Pero si luego quieres playlists por categorías,
+// aquí lo agregamos sin problema 💗.
+console.log("Música Valeris cargada correctamente ✨");
